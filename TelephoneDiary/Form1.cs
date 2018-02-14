2e895582fef0608dc9e17d0def859155bd1dad80
mostafa-tharwat <33877788+mostafa-tharwat@users.noreply.github.com>
2018-02-14 06:39:04 +0200

    first commit

---
+++ TelephoneDiary/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+namespace TelephoneDiary
+{
+    public partial class Form1 : MaterialSkin.Controls.MaterialForm//باستدعي المكتبه الي غيرتلي الشكل 
+    {
+        public object AppData { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            var skinManager = MaterialSkinManager.Instance;
+            SkinManager.AddFormToManage(this);
+            SkinManager.Theme = MaterialSkinManager.Themes.DARK;
+            SkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.BLACK);
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'appData.PhoneBooks' table. You can move, or remove it, as needed.
+            this.phoneBooksTableAdapter.Fill(this.appData.PhoneBooks);
+            Edit(false);
+        }
+        private void Edit(bool value)
+        {
+            txtPhoneNumber.Enabled = value;
+            txtFullName.Enabled = value;
+            txtEmail.Enabled = value;
+            txtAddress.Enabled = value;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Edit(true);
+                appData.PhoneBooks.AddPhoneBooksRow(appData.PhoneBooks.NewPhoneBooksRow());
+                phoneBooksBindingSource.MoveLast();
+                txtPhoneNumber.Focus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                appData.PhoneBooks.RejectChanges();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Edit(true);
+            txtPhoneNumber.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Edit(false);
+            phoneBooksBindingSource.ResetBindings(false);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Edit(false);
+                phoneBooksBindingSource.EndEdit();
+                phoneBooksTableAdapter.Update(appData.PhoneBooks);
+                dataGridView.Refresh();
+                txtPhoneNumber.Focus();
+                MessageBox.Show("Your Data Has Been Successfully Saved .", "message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                appData.PhoneBooks.RejectChanges();
+            }
+        }
+
+        private void dataGridView_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar==(char)13)//Enter
+            {
+                if (MessageBox.Show("Are You Sure Want To Delete This Record ? ", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    phoneBooksBindingSource.RemoveCurrent();
+            }
+
+        }
+    }
+}
